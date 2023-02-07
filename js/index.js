27fcda7532311d06c062b22f4fdc504db8098de9
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-02-08 00:00:36 +0900

    update(2023/2/8)
    메인 화면 중간 안 녕 ?
    부분 추가

---
+++ js/index.js
@@ -0,0 +1,36 @@
+let seconds = 1;
+let timerId;
+timerId = setInterval(function(){
+	console.log(seconds++)
+	if(seconds > 15){
+		clearInterval(timerId)
+	}
+	
+	if(seconds == 2){
+		document.getElementById("move").innerHTML = "안녕";
+	}
+	
+	if(seconds == 3){
+		document.getElementById("move").innerHTML = "こんにちは";
+	}
+	
+	if(seconds == 4){
+		document.getElementById("move").innerHTML = "أهلاً";
+	}
+	
+	if(seconds == 5){
+		document.getElementById("move").innerHTML = "Szia";
+	}
+	
+	if(seconds == 6){
+		document.getElementById("move").innerHTML = "CIAO";
+	}
+	
+	if(seconds == 7){
+		document.getElementById("move").innerHTML = "сайн yy";
+	}
+	
+	if(seconds == 8){
+		document.getElementById("move").innerHTML = "안 녕 ?";
+	}
+}, 300);
