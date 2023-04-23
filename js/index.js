22a0f1aa79031c7c213fd185fb38c2506633a2f6
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-04-23 15:44:17 +0900

    qwe

--- js/index.js
+++ js/index.js
@@ -2,39 +2,39 @@ let seconds = 1;
 let timerId;
 timerId = setInterval(function(){
 	console.log(seconds++)
-	if(seconds > 10){
+	if(seconds > 11){
 		clearInterval(timerId)
 	}
 	
-	if(seconds == 2){
+	if(seconds == 3){
 		document.getElementById("move").innerHTML = "Il linguaggio C è fantastico!";
 	}
 	
-	if(seconds == 3){
+	if(seconds == 4){
 		document.getElementById("move").innerHTML = "C language is great!";
 	}
 	
-	if(seconds == 4){
+	if(seconds == 5){
 		document.getElementById("move").innerHTML = "C-Sprache ist großartig!";
 	}
 	
-	if(seconds == 5){
+	if(seconds == 6){
 		document.getElementById("move").innerHTML = "C-språk er flott!";
 	}
 	
-	if(seconds == 6){
+	if(seconds == 7){
 		document.getElementById("move").innerHTML = "لغة C رائعة!";
 	}
 	
-	if(seconds == 7){
+	if(seconds == 8){
 		document.getElementById("move").innerHTML = "Le langage C est génial!";
 	}
 	
-	if(seconds == 8){
+	if(seconds == 9){
 		document.getElementById("move").innerHTML = "El lenguaje C es genial!";
 	}
 
-    if(seconds == 9){
+    if(seconds == 10){
         document.getElementById("move").innerHTML = "C언어는 위대하다!";
     }
 }, 300);
