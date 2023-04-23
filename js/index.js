a7983ed4fb7a9cbd88c2e8f1c644fe0e0c1dce41
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-04-23 11:39:42 +0900

    UPDATE

--- js/index.js
+++ js/index.js
@@ -1,36 +1,36 @@
-let seconds = 1;
-let timerId;
-timerId = setInterval(function(){
-	console.log(seconds++)
-	if(seconds > 9){
-		clearInterval(timerId)
-	}
-	
-	if(seconds == 2){
-		document.getElementById("move").innerHTML = "MERHABA";
-	}
-	
-	if(seconds == 3){
-		document.getElementById("move").innerHTML = "こんにちは";
-	}
-	
-	if(seconds == 4){
-		document.getElementById("move").innerHTML = "أهلاً";
-	}
-	
-	if(seconds == 5){
-		document.getElementById("move").innerHTML = "Szia";
-	}
-	
-	if(seconds == 6){
-		document.getElementById("move").innerHTML = "CIAO";
-	}
-	
-	if(seconds == 7){
-		document.getElementById("move").innerHTML = "сайн yy";
-	}
-	
-	if(seconds == 8){
-		document.getElementById("move").innerHTML = "안 녕 ?";
-	}
-}, 300);
+// let seconds = 1;let seconds = 1;
+// let timerId;let timerId;
+// timerId = setInterval(function(){timerId = setInterval(function(){
+// 	console.log(seconds++)	console.log(seconds++)
+// 	if(seconds > 9){	if(seconds > 9){
+// 		clearInterval(timerId)		clearInterval(timerId)
+// 	}	}
+// 		
+// 	if(seconds == 2){	if(seconds == 2){
+// 		document.getElementById("move").innerHTML = "MERHABA";		document.getElementById("move").innerHTML = "MERHABA";
+// 	}	}
+// 		
+// 	if(seconds == 3){	if(seconds == 3){
+// 		document.getElementById("move").innerHTML = "こんにちは";		document.getElementById("move").innerHTML = "こんにちは";
+// 	}	}
+// 		
+// 	if(seconds == 4){	if(seconds == 4){
+// 		document.getElementById("move").innerHTML = "أهلاً";		document.getElementById("move").innerHTML = "أهلاً";
+// 	}	}
+// 		
+// 	if(seconds == 5){	if(seconds == 5){
+// 		document.getElementById("move").innerHTML = "Szia";		document.getElementById("move").innerHTML = "Szia";
+// 	}	}
+// 		
+// 	if(seconds == 6){	if(seconds == 6){
+// 		document.getElementById("move").innerHTML = "CIAO";		document.getElementById("move").innerHTML = "CIAO";
+// 	}	}
+// 		
+// 	if(seconds == 7){	if(seconds == 7){
+// 		document.getElementById("move").innerHTML = "сайн yy";		document.getElementById("move").innerHTML = "сайн yy";
+// 	}	}
+// 		
+// 	if(seconds == 8){	if(seconds == 8){
+// 		document.getElementById("move").innerHTML = "안 녕 ?";		document.getElementById("move").innerHTML = "안 녕 ?";
+// 	}	}
+// }, 300);}, 300);
