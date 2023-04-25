5ab7f02930019704c7fe4bed64574108056fe7c0
김레생 <85009117+kimrasng@users.noreply.github.com>
2023-04-26 00:07:24 +0900

    dfdjsoklfhjdsklfjdskl

--- js/index.js
+++ js/index.js
@@ -1,40 +1,40 @@
-let seconds = 1;
-let timerId;
-timerId = setInterval(function(){
-	console.log(seconds++)
-	if(seconds > 11){
-		clearInterval(timerId)
-	}
+// let seconds = 1;
+// let timerId;
+// timerId = setInterval(function(){
+// 	console.log(seconds++)
+// 	if(seconds > 11){
+// 		clearInterval(timerId)
+// 	}
 	
-	if(seconds == 3){
-		document.getElementById("move").innerHTML = "Il linguaggio C è fantastico!";
-	}
+// 	if(seconds == 3){
+// 		document.getElementById("move").innerHTML = "Il linguaggio C è fantastico!";
+// 	}
 	
-	if(seconds == 4){
-		document.getElementById("move").innerHTML = "C language is great!";
-	}
+// 	if(seconds == 4){
+// 		document.getElementById("move").innerHTML = "C language is great!";
+// 	}
 	
-	if(seconds == 5){
-		document.getElementById("move").innerHTML = "C-Sprache ist großartig!";
-	}
+// 	if(seconds == 5){
+// 		document.getElementById("move").innerHTML = "C-Sprache ist großartig!";
+// 	}
 	
-	if(seconds == 6){
-		document.getElementById("move").innerHTML = "C-språk er flott!";
-	}
+// 	if(seconds == 6){
+// 		document.getElementById("move").innerHTML = "C-språk er flott!";
+// 	}
 	
-	if(seconds == 7){
-		document.getElementById("move").innerHTML = "لغة C رائعة!";
-	}
+// 	if(seconds == 7){
+// 		document.getElementById("move").innerHTML = "لغة C رائعة!";
+// 	}
 	
-	if(seconds == 8){
-		document.getElementById("move").innerHTML = "Le langage C est génial!";
-	}
+// 	if(seconds == 8){
+// 		document.getElementById("move").innerHTML = "Le langage C est génial!";
+// 	}
 	
-	if(seconds == 9){
-		document.getElementById("move").innerHTML = "El lenguaje C es genial!";
-	}
+// 	if(seconds == 9){
+// 		document.getElementById("move").innerHTML = "El lenguaje C es genial!";
+// 	}
 
-    if(seconds == 10){
-        document.getElementById("move").innerHTML = "C언어는 위대하다!";
-    }
-}, 300);
+//     if(seconds == 10){
+//         document.getElementById("move").innerHTML = "C언어는 위대하다!";
+//     }
+// }, 300);
